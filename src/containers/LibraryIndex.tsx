f695de5ed79b9c9fa068347243496bb1ce571dc2
JP Yliuntinen <juho-petteri.yliuntinen@luke.fi>
2021-05-20 22:03:12 +0300

    feat: add page and container for model function library

---
+++ src/containers/LibraryIndex.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {ModelFunctionPackage} from '../../types'
+
+export default ({packages}: {packages: ModelFunctionPackage[]}) => {
+    console.log(packages)
+    return (
+    <div>
+        <ul>
+            {packages.map((item: ModelFunctionPackage) => (
+                <li>{item.title}</li>
+            ))}
+        </ul>
+    </div>
+)
+}
