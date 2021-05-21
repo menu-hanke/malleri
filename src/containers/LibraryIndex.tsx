a524247279c54ae7cfb3884da16707c18e6676e2
JP Yliuntinen <juho-petteri.yliuntinen@luke.fi>
2021-05-21 09:33:55 +0300

    fix: add missing key prop for iterable

--- src/containers/LibraryIndex.tsx
+++ src/containers/LibraryIndex.tsx
@@ -6,8 +6,8 @@ export default ({packages}: {packages: ModelFunctionPackage[]}) => {
   return (
     <div>
       <ul>
-        {packages.map((item: ModelFunctionPackage) => (
-          <li>{item.title}</li>
+        {packages.map((item: ModelFunctionPackage, i: number) => (
+          <li key={i}>{item.title}</li>
         ))}
       </ul>
     </div>
