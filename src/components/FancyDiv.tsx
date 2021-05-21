9f7744ace6b90003e2440b24534e5d2758c5c9d6
JP Yliuntinen <juho-petteri.yliuntinen@luke.fi>
2021-05-21 09:31:59 +0300

    fix: added required prop types

--- src/components/FancyDiv.tsx
+++ src/components/FancyDiv.tsx
@@ -1,6 +1,11 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 const FancyDiv: React.FC = ({children}) => {
   return <div style={{border: '1px solid red'}}>{children}</div>;
 };
 export default FancyDiv;
+
+FancyDiv.propTypes = {
+  children: PropTypes.element.isRequired
+};
