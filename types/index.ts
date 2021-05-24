90c79af8ff44b93c96c0f754d650e07eb02c673e
JP Yliuntinen <juho-petteri.yliuntinen@luke.fi>
2021-05-24 12:46:39 +0300

    feat: recreate typescript definitions for model function package

--- types/index.ts
+++ types/index.ts
@@ -4,10 +4,65 @@ export interface Post {
   title: string
 }
 
-export interface ModelFunctionPackage {
-  title: string
+export interface ModelFunctionPublicationDetails {
+  date: string
+  modeledBy: string[]
+  publishedIn: string
+  references: string[]
+}
+
+export interface ModelFunctionTechnicalDetails {
+  writtenBy: string
+  programmingLanguage: string
+  file: string
+  // TODO: model this?
+  dependencies: any
+  siblingModels: any
+}
+
+export interface ModelFunctionInput {
+  name: string
+  varname: string
   description: string
-  developers: string[]
-  authors: string[]
-  active: boolean
+  group: string
+  unit: string
+}
+
+export interface ModelFunctionOutput {
+  varname: string
+  group: string
+  unit: string
+  type: string
+}
+
+export interface ModelFunctionCertificationDetails {
+  certifiedBy: string
+  version: string
+  date: string
+  modified: string
+  validation: object
+  calibration: any
+}
+
+export interface ModelFunctionGeneralDetails {
+  name: string
+  group: string
+  description: string
+  applicableTo: any
+  output: ModelFunctionOutput[]
+  input: ModelFunctionInput[]
+}
+
+export interface ModelFunction {
+  publicationDetails: ModelFunctionPublicationDetails
+  technicalDetails: ModelFunctionTechnicalDetails
+  certificationDetails: ModelFunctionCertificationDetails
+  generalDetails: ModelFunctionGeneralDetails
+  additionalContent: any
+}
+
+export interface ModelFunctionPackage {
+  package: string
+  searchPhrases: string[]
+  models: ModelFunction[]
 }
