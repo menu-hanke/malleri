11726325bd62ce35e14e774d234f58b16d5cdc39
JP Yliuntinen <juho-petteri.yliuntinen@luke.fi>
2021-05-24 13:42:37 +0300

    feat: add title and description filters to LibraryIndex

--- src/containers/LibraryIndex.tsx
+++ src/containers/LibraryIndex.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ModelFunctionPackage} from '../../types';
 
 export interface Props {
@@ -6,10 +6,27 @@ export interface Props {
 }
 
 export default (props: Props) => {
+  // 1. search input and filtering
+  // 2. refactoring & unit testing functionality
+  // 3. new view for model function package
+
+  const [titleFilterInput, setTitleFilterInput] = useState('');
+  const [descriptionFilterInput, setDescriptionFilterInput] = useState('');
+
+  const totalFilter = (item: ModelFunctionPackage) => {
+    return item.title.includes(titleFilterInput) && item.description.includes(descriptionFilterInput);
+  };
+
+  const applyPackagesFiltering = (packages: ModelFunctionPackage[]) => {
+    return packages.filter(totalFilter);
+  };
+
   return (
     <div>
+      <input type="text" placeholder="Filter by title..." onChange={(event) => setTitleFilterInput(event.target.value)} />
+      <input type="text" placeholder="Filter by description..." onChange={(event) => setDescriptionFilterInput(event.target.value)} />
       <ul>
-        {props.packages.map((item: ModelFunctionPackage, i: number) => (
+        {applyPackagesFiltering(props.packages).map((item: ModelFunctionPackage, i: number) => (
           <li key={i}>{item.title}</li>
         ))}
       </ul>
