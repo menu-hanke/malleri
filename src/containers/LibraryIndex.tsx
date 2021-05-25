8a85ec0cd39933f2b3691e7602d0ea205648eedc
JP Yliuntinen <juho-petteri.yliuntinen@luke.fi>
2021-05-25 08:55:00 +0300

    feat: add Modal element to Library view for some package details

--- src/containers/LibraryIndex.tsx
+++ src/containers/LibraryIndex.tsx
@@ -1,9 +1,17 @@
 import React, {useState} from 'react';
+import Modal from 'react-modal';
 import {ModelFunctionPackage} from '../../types';
 
 export interface Props {
   packages: ModelFunctionPackage[]
 }
+
+const placeholderPackageItem = {
+  title: '',
+  description: '',
+  searchPhrases: [],
+  models: []
+};
 
 export default (props: Props) => {
   // 1. search input and filtering
@@ -12,6 +20,7 @@ export default (props: Props) => {
 
   const [titleFilterInput, setTitleFilterInput] = useState('');
   const [descriptionFilterInput, setDescriptionFilterInput] = useState('');
+  const [itemInModal, setItemInModal]: [ModelFunctionPackage, Function] = useState(placeholderPackageItem);
 
   const totalFilter = (item: ModelFunctionPackage) => {
     return item.title.includes(titleFilterInput) && item.description.includes(descriptionFilterInput);
@@ -23,6 +32,22 @@ export default (props: Props) => {
 
   return (
     <div>
+      <Modal
+        isOpen={itemInModal != placeholderPackageItem}
+        onRequestClose={() => setItemInModal(placeholderPackageItem)}
+        contentLabel="Mallifunktiopaketin sisältö"
+      >
+        <b>{itemInModal.title}</b>
+        <p>{itemInModal.description}</p>
+        <p>
+          Mallit:
+          <ul>
+            {itemInModal.models.map((model) =>
+              (<li>{model.generalDetails.name}</li>))}
+          </ul>
+        </p>
+
+      </Modal>
       <input type="text" placeholder="Filter by title..." onChange={(event) => setTitleFilterInput(event.target.value)} />
       <input type="text" placeholder="Filter by description..." onChange={(event) => setDescriptionFilterInput(event.target.value)} />
       <ul>
