4d5f7dd70496b8f88f98110e58ffcb530cee34bc
dmeland <daniel.melander@luke.fi>
2021-06-17 09:33:14 +0300

    feat: MENU-logo on left side of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Root, Routes, addPrefetchExcludes} from 'react-static';
 import {Link, Router} from '@reach/router';
 import {GlobalStyle} from './Styled';
+import Logo from './components/Logo'
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic']);
@@ -11,6 +12,7 @@ function App() {
     <Root>
       <GlobalStyle />
       <nav>
+        <Logo />
         <Link to="/">Homepage</Link>
         <Link to="/library">Library</Link>
         <Link to="/simulator">Mela2.0 software</Link>
